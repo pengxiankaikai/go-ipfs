ad0ddb972c958c056d366221045821956308b418
Kevin Atkinson <k@kevina.org>
2018-11-22 04:16:45 -0500

    Add global --cid-base option and enable it for most commands.
    This does it on ther server side for most commands.
    
    This also adds a global --output-cidv1 option.
    
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- core/commands/ls.go
+++ core/commands/ls.go
@@ -11,6 +11,7 @@ import (
 
 	blockservice "gx/ipfs/QmPoh3SrQzFBWtdGK6qmHDV4EanKR6kYPj4DD3J2NLoEmZ/go-blockservice"
 	cid "gx/ipfs/QmR8BauakNcBa3RbE4nbQu76PDiJgoQgz8AJdhJuiU4TAw/go-cid"
+	cidenc "gx/ipfs/QmVjZoEZg2oxXGFGjbD28x3gGN6ALHAW6BN2LKRUcaJ21i/go-cidutil/cidenc"
 	offline "gx/ipfs/QmYZwey1thDTynSrvd6qQkX24UpTka6TFhQ2v569UpoqxD/go-ipfs-exchange-offline"
 	cmds "gx/ipfs/Qma6uuSyjkecGhMFFLfzyJDPyoDtNJSHJNweDccZhaWkgU/go-ipfs-cmds"
 	ipld "gx/ipfs/QmcKKBwfz6FyQdHR2jsXrrF6XeSBXYL86anmWNewpFpoF5/go-ipld-format"
@@ -91,8 +92,12 @@ The JSON output contains type information.
 		if err != nil {
 			return err
 		}
-
 		paths := req.Arguments
+
+		enc, err := cmdenv.ProcCidBase(req)
+		if err != nil {
+			return err
+		}
 
 		var dagnodes []ipld.Node
 		for _, fpath := range paths {
@@ -131,7 +136,7 @@ The JSON output contains type information.
 				}
 				outputLinks := make([]LsLink, len(links))
 				for j, link := range links {
-					lsLink, err := makeLsLink(req, dserv, resolve, link)
+					lsLink, err := makeLsLink(req, dserv, resolve, link, enc)
 					if err != nil {
 						return err
 					}
@@ -165,7 +170,7 @@ The JSON output contains type information.
 					return linkResult.Err
 				}
 				link := linkResult.Link
-				lsLink, err := makeLsLink(req, dserv, resolve, link)
+				lsLink, err := makeLsLink(req, dserv, resolve, link, enc)
 				if err != nil {
 					return err
 				}
@@ -224,7 +229,7 @@ func makeDagNodeLinkResults(req *cmds.Request, dagnode ipld.Node) <-chan unixfs.
 	return linkResults
 }
 
-func makeLsLink(req *cmds.Request, dserv ipld.DAGService, resolve bool, link *ipld.Link) (*LsLink, error) {
+func makeLsLink(req *cmds.Request, dserv ipld.DAGService, resolve bool, link *ipld.Link, enc cidenc.Interface) (*LsLink, error) {
 	t := unixfspb.Data_DataType(-1)
 
 	switch link.Cid.Type() {
@@ -250,7 +255,7 @@ func makeLsLink(req *cmds.Request, dserv ipld.DAGService, resolve bool, link *ip
 	}
 	return &LsLink{
 		Name: link.Name,
-		Hash: link.Cid.String(),
+		Hash: enc.Encode(link.Cid),
 		Size: link.Size,
 		Type: t,
 	}, nil
